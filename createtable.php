a6600975c8ba0a7c7c86cd1794c1edc60f960d29
Jasper Co <jasper_co_inferno@yahoo.com.ph>
2013-10-21 23:29:48 +0800

    initial commit

---
+++ createtable.php
@@ -0,0 +1,22 @@
+<?php
+// DB connection info
+$host = "tcp:ich9j148bm.database.windows.net,1433";
+$user = "lexidatabase@ich9j148bm";
+$pwd = "January11!";
+$db = "tasklist";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+?>
